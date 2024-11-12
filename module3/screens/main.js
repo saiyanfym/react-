0149c08ebac99e328ed30bf9992fe319c346a9d8
saiyanfym <muratifestim16@gmail.com>
2024-11-12 20:46:16 +0100

    Initial commit

---
+++ module3/screens/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Text, StyleSheet, View} from "react-native";
+const Mainscreen =()=>{
+    return <View>
+        <Text> This is from Main Screen</Text>
+    </View>
+}
+
+export default Mainscreen;
